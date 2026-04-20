964e839246699580d526032b52ade448ce63023f
Taufik Awaludin <user@UserdeMBP.lan>
2026-04-20 14:52:03 +0900

    fix api url

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://rich-udin.onrender.com/api";
 
 // ================= AUTH =================
 const token = localStorage.getItem("token");
